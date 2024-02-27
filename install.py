d00ee5e61b622f75dec5ea832ffb593dd3e6574a
nonogogono <morinaga_dars_stream@yahoo.co.jp>
2024-02-27 20:27:46 +0900

    feat(package): Flask, flask-cors 自動インストール

--- install.py
+++ install.py
@@ -1,4 +1,13 @@
 import launch
 
-if not launch.is_installed("yaml"):
-    launch.run_pip("install yaml", desc='Installing yaml')
+if not launch.is_installed("Flask"):
+    launch.run_pip("install Flask", desc='Flask')
+    print("=========================")
+    print("Flask installed")
+    print("=========================")
+
+if not launch.is_installed("flask-cors"):
+    launch.run_pip("install flask-cors", desc='flask-cors')
+    print("=========================")
+    print("flask-cors installed")
+    print("=========================")
